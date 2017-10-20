b6ac749bd9cee5de71dea15e6a243c4350144317
Karim Slimani <karim@playmedia.fr>
2017-10-20 15:54:42 +0200

    test: add Geo class tests

---
+++ tests/GeoTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests;
+
+use Tests\CreatesConfig;
+use Money\Exchange\SwapExchange;
+use Sk\Geo\Geo;
+use Sk\Geo\Locale;
+use Sk\Geo\Location\MaxmindLocation;
+use Sk\Geo\Money;
+use Swap\Builder;
+
+class GeoTest extends \PHPUnit_Framework_TestCase
+{
+    use CreatesConfig;
+
+    protected $geo;
+
+    protected function setUp()
+    {
+        $basePath = realpath(__DIR__.'/../');
+        $config = $this->CreateConfig();
+        $exchange = new SwapExchange((new Builder())->add('fixer')->build());
+
+        $this->geo = new Geo(
+            new Locale($config, $basePath),
+            new MaxmindLocation($config),
+            new Money($config, $exchange)
+        );
+    }
+
+    public function test_it_access_by_methods()
+    {
+        $this->assertInstanceOf(Locale::class, $this->geo->locale());
+        $this->assertInstanceOf(MaxmindLocation::class, $this->geo->location());
+        $this->assertInstanceOf(Money::class, $this->geo->money());
+    }
+
+    public function test_it_access_by_properties()
+    {
+        $this->assertInstanceOf(Locale::class, $this->geo->locale);
+        $this->assertInstanceOf(MaxmindLocation::class, $this->geo->location);
+        $this->assertInstanceOf(Money::class, $this->geo->money);
+    }
+}
